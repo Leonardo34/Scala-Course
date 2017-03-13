723fdd2fb2531472d7c8bdeddc20f30362d5c7f1
Leonardo34 <leo_broch@hotmail.com>
2017-03-13 17:45:07 -0300

    altered the visibility of auxiliar functions in sqrt function and init the recursion exercise

---
+++ src/week01/RecursionExercise.scala
@@ -0,0 +1,11 @@
+package week01
+
+object RecursionExercise {
+  def main(args: List[String]) = {
+    
+  }
+  
+  def pascal(c: Int, r: Int): Int = {
+    1 
+  }
+}
--- src/week01/Sqrt.scala
+++ src/week01/Sqrt.scala
@@ -1,26 +1,25 @@
 package week01
 
 object Sqrt {
-  def abs(x: Double) = if (x < 0) -x else x
-  
-  def sqrtIter(guess: Double, x: Double): Double = {
-    if (isGoodEnough(guess, x)) guess
-    else sqrtIter(improve(guess, x), x)
-  }
-  
-  def isGoodEnough(guess: Double, x: Double) = {
-    abs((guess * guess - x)) < 0.001
-  }
-  
-  def improve(guess: Double, x: Double) = {
-    (guess + x / guess) / 2
-  }
-  
   def sqrt(x: Double): Double = {
+    def abs(x: Double) = if (x < 0) -x else x
+
+    def sqrtIter(guess: Double, x: Double): Double = {
+      if (isGoodEnough(guess, x)) guess
+      else sqrtIter(improve(guess, x), x)
+    }
+
+    def isGoodEnough(guess: Double, x: Double) = {
+      abs((guess * guess - x)) < 0.001
+    }
+
+    def improve(guess: Double, x: Double) = {
+      (guess + x / guess) / 2
+    }
     sqrtIter(1.0, x)
   }
   
   def main(args: Array[String]) {
-    println(sqrt(2))
+    println(sqrt(100))
   }
 }
